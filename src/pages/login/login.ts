f8fe140dc458e3fa7686b35cf70e3a58071f8689
Mhlanga <smhlanga@deloitte.co.za>
2019-12-19 21:23:48 +0200

    initial code

---
+++ src/pages/login/login.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  ToastController
+} from 'ionic-angular';
+import { TabsPage } from '../tabs/tabs';
+
+@Component({
+  selector: 'login',
+  templateUrl: 'login.html'
+})
+export class LoginPage {
+  email: any;
+  password: any;
+
+  public constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public toastCtrl: ToastController
+  ) {}
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+  routeToTabs(){
+    this.navCtrl.push(TabsPage);
+  }
+
+}
